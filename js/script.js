758d2b92aa11e6e34c0bee6967f9151cd9daad50
Muhammad Fadlan Ardhiansyah <mfadlanardhiansyah.work@gmail.com>
2025-10-03 07:53:50 +0700

    lenis more heavy

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ if (e.key === 'Escape') closeSidebarFn();
 
 // Lenis smooth scroll
 const lenis = new Lenis({
-duration: 1.2,      
+duration: 1.4,      
 easing: t => 1 - Math.pow(1 - t, 3),
 smooth: true,
 });
